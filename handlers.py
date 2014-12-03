4c025db2581ddb93125c0c98410ed144a187bac6
djohnson <mr.demarcus.johnson@gmail.com>
2014-12-03 14:46:42 -0600

    commands - testing the /? function, testing complete, removing extra code.

--- handlers.py
+++ handlers.py
@@ -106,7 +106,6 @@ class MessageHandler(object):
                 # Run command
                 if event.text[1] == '?':
                     event.text = "/help"
-                    self.bot.send_message(event.conv, event.text)
                 yield from self.handle_command(event)
             else:
                 # Forward messages
