d0cab22894214070e7ed5a919805446f38244ce4
DJ <mr.demarcus.johnson@gmail.com>
2014-12-05 22:52:26 -0600

    commands -marco - adding back.

--- commands.py
+++ commands.py
@@ -272,6 +272,10 @@ def ping(bot, event, *args):
     else:
         bot.send_message(event.conv, 'pong')
 
+@command.register
+def marco(bot, event, *args):
+    bot.send_message(event.conv, 'polo')
+
 
 @command.register
 def echo(bot, event, *args):
