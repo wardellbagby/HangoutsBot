0ba80b7d14a088ad07d19389ef24578d3c26f096
Wardell Chandler <wardell.chandler@gmail.com>
2015-03-02 12:37:05 -0600

    Fixes #7: Adds in new functionality to fix issue with the bot not being able to find the config file after the refactor. It will also now add in the default config file at Core/config.json if it can't find it in the root folder or the Core folder.

--- Main.py
+++ Main.py
@@ -1,20 +1,42 @@
-# Add new /<command name> based commands in commands.py
-# Add any other new commands in Handlers.py handle function.
-
-
-# Useless function? Maybe...
 import os
 
 from Core.Bot import HangoutsBot
 
-
-class Main:
-    @staticmethod
-    def start():
-        # This commands auto updates the project. Please have Git installed and in your PATH variable on Windows.
-        os.system("git pull")
-        HangoutsBot("Core" + os.sep + "cookies.txt", "Core" + os.sep + "config.json").run()
-
+base_config = '''{
+  "admins": ["YOUR-USER-ID-HERE"],
+  "autoreplies_enabled": true,
+  "autoreplies": [
+    [["bot", "robot", "Yo"], "/think {}"]
+  ],
+  "development_mode": false,
+  "commands_admin": ["hangouts", "reload", "quit", "restart", "config", "restart", "block"],
+  "commands_enabled": true,
+  "forwarding_enabled": false,
+  "rename_watching_enabled": true,
+  "conversations": {
+    "CONV-ID-HERE": {
+      "autoreplies": [
+        [["whistle", "bot", "whistlebot"], "/think {}"],
+        [["trash"], "You're trash"]
+      ],
+      "forward_to": [
+        "CONV1_ID"
+      ]
+    }
+  }
+}'''
 
 if __name__ == "__main__":
-    Main().start()
+
+    # This commands auto updates the project. Please have Git installed and in your PATH variable on Windows.
+    os.system("git pull")
+    if os.path.isfile("config.json"):
+        HangoutsBot("cookies.txt", "config.json").run()
+    elif os.path.isfile("Core" + os.sep + "config.json"):
+        HangoutsBot("Core" + os.sep + "cookies.txt", "Core" + os.sep + "config.json").run()
+    else:
+        print("Error finding config.json file. Creating default config file in at Core/config.json")
+        config_file = open("Core" + os.sep + "config.json", 'w+')
+        config_file.writelines(base_config)
+        config_file.close()
+        HangoutsBot("Core" + os.sep + "cookies.txt", "Core" + os.sep + "config.json").run()
