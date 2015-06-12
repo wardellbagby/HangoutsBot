62fdff729e84a795d5cc6c5ae159a3fb32ac2842
Wardell Chandler <wardell.chandler@gmail.com>
2015-06-12 15:21:53 -0500

    Fixes issue with default karma increasing autoreply not working.

--- Core/Autoreplies/DefaultAutoreplies.py
+++ Core/Autoreplies/DefaultAutoreplies.py
@@ -7,6 +7,6 @@ url_regex = "^((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\
 DispatcherSingleton.register_autoreply(
     AutoReply(["whistle", "bot", "whistlebot"], "/think {}", label="Automatic Replies"))
 DispatcherSingleton.register_autoreply(AutoReply(["^@[\\w\\s]+$"], "/karma {}", label="Karma Status"))
-DispatcherSingleton.register_autoreply(AutoReply(["^@[\\w\\s]+\\++$"], "/karma {}", label="Karma Increasing"))
+DispatcherSingleton.register_autoreply(AutoReply(["^@[\\w\\s]+\\++$"], "/_karma {}", label="Karma Increasing"))
 DispatcherSingleton.register_autoreply(AutoReply(["^@[\\w\\s]+-+$"], "/_karma {}", label="Karma Decreasing"))
 DispatcherSingleton.register_autoreply(AutoReply([url_regex], "/_karma {}", label="Link Summarizer"))
